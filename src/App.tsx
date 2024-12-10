3789655c5d95a8d6b1498a2de5de888e9156f31e
patel-prince <prpatel0949@gmail.com>
2024-12-10 07:26:20 +0000

    add counter example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useState } from 'react'
+
 const App = () => {
-  return <div>Kreattix Templated App</div>
+  const [count, setCount] = useState<number>(0)
+
+  function increament() {
+    setCount((prev) => prev + 1)
+  }
+
+  return <button onClick={increament}>Count is {count}</button>
 }
 
 export default App
